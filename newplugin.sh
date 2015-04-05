59a4544b26cfbe02ee973219b7a1e050f208e190
danielo515 <rdanielo@gmail.com>
2015-04-05 10:18:53 +0200

    Some security checks on new plugin creation

--- newplugin.sh
+++ newplugin.sh
@@ -3,6 +3,9 @@
 # It will create a new wiki-folder with a skeleton of your plugin to start with
 # Then copy that folder to wherever you want to develop your new plugin
 read -p "Name the new plugin: " pluginname
+if [ -z "$pluginname" ]; then
+    exit -1
+fi
 pluginwiki=${pluginname}wiki
 pluginfolder=$pluginwiki/plugins/$pluginname
 cp -r *wiki $pluginwiki
